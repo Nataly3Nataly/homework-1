d589a6e09398edc0d37ba1e2d8fb4872227e94f0
Nataliia Nosova <nataliianosova2000@gmail.com>
2024-02-15 11:07:11 +0200

    age promt changed

--- file.js
+++ file.js
@@ -3,11 +3,11 @@ if(!isNaN(year) && year >= 0){
     let endOfDigist = year % 10;
     if (year !== 12 && year !== 14 && (endOfDigist === 2 || endOfDigist === 4)){
     confirm("Ваш вік - " + year + " роки");
-    }else if (endOfDigist === 1) {
+    } else if (endOfDigist === 1) {
         confirm("Ваш вік - " + year + " рік");
-    }else {
+    } else {
         confirm("Ваш вік - " + year + " років");}
     }
 else{
-    year = prompt('Спробуйте ще раз! Ввведіть свій вік (тільки цифри)');
+   alert('Ви додали невалідний вік. Спробуйте ще раз!');
 }
