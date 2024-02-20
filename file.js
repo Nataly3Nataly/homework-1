413cbc4d10036f451f96bcc9840a4173a30fe28f
Nataliia Nosova <nataliianosova2000@gmail.com>
2024-02-20 17:29:24 +0200

    created second and third task

--- file.js
+++ file.js
@@ -1,19 +1,38 @@
-function drawTriangle(amount, star) {
-  let line = "";
-  for (let i = 1; i <= amount; i++) {
-    line += star;
-    console.log(line);
-  }
-}
-drawTriangle(5, "*");
+// HomeWork 1
 
-function triangleDraw(amount, star) {
-  let line = "";
-  let i = 0;
-  while (i <= amount) {
-    i++;
-    line += star;
-    console.log(line);
-  }
-}
-triangleDraw(5, "*");
+// function drawTriangle(amount, star) {
+//   let line = "";
+//   for (let i = 1; i <= amount; i++) {
+//     line += star;
+//     console.log(line);
+//   }
+// }
+// drawTriangle(5, "*");
+
+// function triangleDraw(amount, star) {
+//   let line = "";
+//   let i = 0;
+//   while (i <= amount) {
+//     i++;
+//     line += star;
+//     console.log(line);
+//   }
+// }
+// triangleDraw(5, "*");
+
+// HomeWork 2
+// function sum() {
+//   let sum = 0;
+//   for (let i = 1; i <= 100; i++) {
+//     if (i % 3 !== 0);
+//     sum += i;
+//   }
+//   return console.log(sum);
+// }
+// sum();
+
+//HomeWork3
+// function pow(x, y) {
+//   return Math.pow(x, y);
+// }
+// console.log(pow(2, 3));
