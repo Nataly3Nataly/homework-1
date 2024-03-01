e37df327a458f35869db6ca6b32ad9fab6f10e2c
Nataliia Nosova <nataliianosova2000@gmail.com>
2024-03-01 18:39:06 +0200

    Added tasks

---
+++ file.js
@@ -0,0 +1,30 @@
+//Task 1
+// let obj = {
+//   name: "John",
+//   age: 25,
+//   city: "New York",
+
+//   getInfo: function () {
+//     for (let key in this) {
+//       if (typeof this[key] !== "function") {
+//         console.log(key + ": " + this[key]);
+//       }
+//     }
+//   },
+// };
+// obj.getInfo();
+// obj.gender = "Male";
+// obj.getInfo();
+
+//Task 2
+// let services = {
+//   стрижка: 60,
+//   гоління: 80,
+//   "Миття голови": 100,
+// };
+// services["Розбити скло"] = 200;
+// var totalCost = 0;
+// for (var key in services) {
+//   totalCost += services[key];
+// }
+// console.log("Вартість наданних послуг: " + totalCost + " грн");
